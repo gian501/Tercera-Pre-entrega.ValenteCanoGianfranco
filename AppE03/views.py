a53d9e99d4d667792567f4e4e5ed1cc43e07c8eb
Gianfranco Valente Cano <gianfrancovalentecano@gmail.com>
2023-02-14 11:10:55 -0300

    Checkpoint.modificacion de templates, views

--- AppE03/views.py
+++ AppE03/views.py
@@ -4,22 +4,20 @@ from AppE03.models import Curso
 from AppE03.models import Profesor
 from AppE03.models import Estudiante
 from AppE03.models import Entregable
-'''
-from AppC.forms import CursoFormulario
-from AppC.forms import ProfesoresFormulario
-from AppC.forms import EstudiantesFormulario
-from AppC.forms import EntregableFormulario
-'''
+from AppE03.forms import CursoFormulario
+from AppE03.forms import ProfesoresFormulario
+from AppE03.forms import EstudiantesFormulario
+from AppE03.forms import EntregableFormulario
+
 # Create your views here.
 
 
 def inicio(request):
-    
     return render(request, "AppE03/inicio.html", )
-    #return HttpResponse('Vista Inicio')
+    
 
 def cursos(request):
-    '''
+
     if request.method == "POST":
  
         miFormulario = CursoFormulario(request.POST) # Aqui me llega la informacion del html
@@ -29,17 +27,15 @@ def cursos(request):
             informacion = miFormulario.cleaned_data
             curso = Curso(nombre=informacion["curso"], comision=informacion["comision"])
             curso.save()
-            return render(request, "AppC/cursos.html" ) #ubicacion a donde me lleva
+            return render(request, "AppE03/cursos.html" ) #ubicacion a donde me lleva
     else:
         miFormulario = CursoFormulario()
         
-    return render(request, 'AppC/cursos.html',{"miFormulario": miFormulario})
-    '''
-    return render(request, 'AppE03/inicio.html')
-    #return HttpResponse('Vista cursos')
+    return render(request, 'AppE03/cursos.html',{"miFormulario": miFormulario})
+    
 
 def profesores(request):
-    '''
+    
     if request.method == "POST":
  
         miFormulario = ProfesoresFormulario(request.POST) # Aqui me llega la informacion del html
@@ -49,19 +45,13 @@ def profesores(request):
             informacion = miFormulario.cleaned_data
             profesor = Profesor(nombre=informacion["nombre"], apellido=informacion["apellido"],email = informacion["e-mail"], profesion = informacion['profesion'])
             profesor.save()
-        return render(request, "AppC/profesores.html" ) #ubicacion a donde me lleva
+        return render(request, "AppE03/profesores.html" ) #ubicacion a donde me lleva
     else:
         miFormulario = ProfesoresFormulario()
         
-    return render(request, 'AppC/profesores.html',{"miFormulario": miFormulario})
-    '''
-    return render(request, 'AppE03/profesores.html')
-    #return HttpResponse('Vista profesores')
-
-
+    return render(request, 'AppE03/profesores.html',{"miFormulario": miFormulario})
 
 def estudiantes(request):
-    '''
     if request.method == "POST":
  
         miFormulario = EstudiantesFormulario(request.POST) # Aqui me llega la informacion del html
@@ -71,17 +61,15 @@ def estudiantes(request):
             informacion = miFormulario.cleaned_data
             estudiante = Estudiante(nombre=informacion["nombre"], apellido=informacion["apellido"],email = informacion["e-mail"])
             estudiante.save()
-        return render(request, "AppC/estudiantes.html" ) #ubicacion a donde me lleva
+        return render(request, "AppE03/estudiantes.html" ) #ubicacion a donde me lleva
     else:
         miFormulario = EstudiantesFormulario()
 
-    return render(request, 'AppC/estudiantes.html',{"miFormulario": miFormulario})
-    '''
-    return render(request, 'AppE03/estudiantes.html')
-    #return HttpResponse('Vista estudiantes')
+    return render(request, 'AppE03/estudiantes.html',{"miFormulario": miFormulario})
+
 
 def entregables(request):
-    '''
+   
     if request.method == "POST":
  
         miFormulario = EntregableFormulario(request.POST) # Aqui me llega la informacion del html
@@ -91,11 +79,9 @@ def entregables(request):
             informacion = miFormulario.cleaned_data
             entregable = Entregable (nombre=informacion["nombre"],apellido=informacion["apellido"],email = informacion["e-mail"], fechadeentrega=informacion["Fecha de entrega"],entregado = informacion["entregado"])
             entregable.save()
-        return render(request, "AppC/entregables.html" ) #ubicacion a donde me lleva
+        return render(request, "AppE03/entregables.html" ) #ubicacion a donde me lleva
     else:
         miFormulario = EntregableFormulario()
 
-    return render(request, 'AppC/entregables.html',{"miFormulario": miFormulario})
-    '''
-    return render(request, 'AppE03/entregables.html')
-    #return HttpResponse('Vista entregables')
+    return render(request, 'AppE03/entregables.html',{"miFormulario": miFormulario})
+
