65e12dd1770867a950b6f80e9c4dc6beb152e853
Gianfranco Valente Cano <gianfrancovalentecano@gmail.com>
2023-02-15 12:01:45 -0300

    Checkpoint.Appfuncionando

--- Preentrega03/urls.py
+++ Preentrega03/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('AppE03/', include('AppE03.urls')),
 ]
