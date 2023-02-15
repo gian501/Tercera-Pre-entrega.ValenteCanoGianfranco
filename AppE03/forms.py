97ec7759c912825b85a755bd314d37db84987c9c
Gianfranco Valente Cano <gianfrancovalentecano@gmail.com>
2023-02-15 09:34:33 -0300

    Checkponit. todo funciona, ahora modificar para la creacion de busqueda

--- AppE03/forms.py
+++ AppE03/forms.py
@@ -2,13 +2,13 @@ from django import forms
 
 class CursoFormulario(forms.Form):
     curso = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Curso'}))
-    comision = forms.IntegerField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Curso'}))
+    comision = forms.IntegerField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Comision'}))
 
 class ProfesoresFormulario(forms.Form):
     nombre = forms.CharField(max_length=40, required=True, widget=forms.TextInput(attrs={'placeholder': 'Nombre'})) 
     apellido = forms.CharField(max_length=40, required=True, widget=forms.TextInput(attrs={'placeholder': 'Apellido'})) 
     email = forms.EmailField(max_length=40, required=True, widget=forms.TextInput(attrs={'placeholder': 'Email'})) 
-    profesion = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    profesion = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'Profesion'}))
 class EstudiantesFormulario(forms.Form):
     nombre = forms.CharField(max_length=40, required=True, widget=forms.TextInput(attrs={'placeholder': 'Nombre'})) 
     apellido = forms.CharField(max_length=40, required=True, widget=forms.TextInput(attrs={'placeholder': 'Apellido'})) 
@@ -18,6 +18,6 @@ class EntregableFormulario(forms.Form):
     nombre = forms.CharField(max_length=40, required=True, widget=forms.TextInput(attrs={'placeholder': 'Nombre'})) 
     apellido = forms.CharField(max_length=40, required=True, widget=forms.TextInput(attrs={'placeholder': 'Apellido'})) 
     email = forms.EmailField(max_length=40, required=True, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
-    fechaEntrega = forms.DateTimeField()
+    fechaEntrega = forms.DateTimeField(required=True, widget=forms.TextInput(attrs={'placeholder': '14-02-2023'}))
     entregado = forms.BooleanField()
 
